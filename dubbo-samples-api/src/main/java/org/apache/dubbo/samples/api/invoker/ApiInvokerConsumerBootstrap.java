4e0bb65471eb64773da1949643842428aafd98e7
497291644@qq.com <16&8gp$p=1Jj>
2022-03-10 00:05:38 +0800

    ReferenceConfig

--- dubbo-samples-api/src/main/java/org/apache/dubbo/samples/api/invoker/ApiInvokerConsumerBootstrap.java
+++ dubbo-samples-api/src/main/java/org/apache/dubbo/samples/api/invoker/ApiInvokerConsumerBootstrap.java
@@ -26,14 +26,22 @@ import com.alibaba.dubbo.config.RegistryConfig;
 public class ApiInvokerConsumerBootstrap {
     public static void main(String[] args) {
         System.setProperty("java.net.preferIPv4Stack", "true");
+        // 这里还有个范型呢
         ReferenceConfig<HelloService> reference = new ReferenceConfig<>();
+        // applicationConfig, 仅仅只是设置了一下, 服务的名称而已
         ApplicationConfig application = new ApplicationConfig("first-dubbo-consumer");
         application.setQosPort(3333);
+
+        // 设置到它爷爷的属性里去了
         reference.setApplication(application);
+        // 设置注册中心的配置, 也是设置到它爷爷里的属性去了
         reference.setRegistry(new RegistryConfig("zookeeper://127.0.0.1:2181"));
 //        reference.setRegistry(new RegistryConfig("multicast://224.5.6.7:1234"));
+        // 设置去引用什么类
         reference.setInterface(HelloService.class);
+        // 去引用配置中获取类
         HelloService helloService = reference.get();
+
         String message = helloService.sayHello("dubbo");
         System.err.println(message);
     }
